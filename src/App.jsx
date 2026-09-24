7424ff83948364f42c254a18cfbc79659edfc428
Krish <krishppatil5471@gmail.com>
2026-09-24 20:52:45 +0530

    Add Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
         <About />
         <Education />
         <Skills />
+        {/*Projects section */}
         <Projects />
         <Certificates />
         <Achievements />
